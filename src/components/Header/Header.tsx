9f59b69698f60613e4b0077fa0652be698c09067
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-03-06 08:08:51 -0500

    Change readme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 import {
@@ -11,23 +11,34 @@ import {
 import logo from "../../assets/images/logo.png";
 
 const Header: FC = () => {
+  const [search, setSearch] = useState<string>("");
+
   return (
     <HeaderContainer>
       <div style={{ display: "flex", alignItems: "center" }}>
-        <Logo src={logo} />
+        <Logo src={logo} title="Crypto Bob" />
         <InputContainer>
           <FaSearch style={{ margin: 15 }} />
-          <SearchInput placeholder="Search what you're lookin for..." />
+          <SearchInput
+            placeholder="Search what you're lookin for..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </InputContainer>
       </div>
       <div>
         <MenuItem href="mailto:abolfazlpanahiazar@gmail.com">
           Contact Me
         </MenuItem>
-        <MenuItem href="https://linkedin.com/in/abolfazlpanahiazar/" target="_blank">
+        <MenuItem
+          href="https://linkedin.com/in/abolfazlpanahiazar/"
+          target="_blank"
+        >
           Linkedin
         </MenuItem>
-        <MenuItem href="https://github.com/abolfazlPanahiazar" target="_blank">Github</MenuItem>
+        <MenuItem href="https://github.com/abolfazlPanahiazar" target="_blank">
+          Github
+        </MenuItem>
       </div>
     </HeaderContainer>
   );
